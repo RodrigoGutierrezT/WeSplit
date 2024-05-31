873e41f7c5848bf98d71cdcf197cd9fe80b4dbb6
RodrigoGutierrezT <rodrigo644@gmail.com>
2024-05-30 20:13:59 -0400

    Add Spliting Bill functionality

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,74 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by Rodrigo on 28-05-24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount: Double = 0.0
+    @State private var numberOfPeople: Int = 2
+    @State private var tipPercentage: Int = 20
+    @FocusState private var amountIsFocused: Bool
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var grandTotal: Double {
+        checkAmount + (checkAmount / 100 * Double(tipPercentage))
+    }
+    
+    var amountPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        
+        return grandTotal / peopleCount
+    }
+    
+    var body: some View {
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                        .keyboardType(.decimalPad)
+                        .focused($amountIsFocused)
+                    
+                    Picker("Number of People", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+                
+                Section("How much do you want to tip?") {
+                    Picker("Tip amount", selection: $tipPercentage){
+                        ForEach(tipPercentages, id: \.self) {
+                            Text($0, format: .percent)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Total + Tip") {
+                    Text(grandTotal, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+                
+                Section("Amount per Person") {
+                    Text(amountPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                }
+            }
+            .navigationTitle("WeSplit")
+            .toolbar {
+                if amountIsFocused {
+                    Button("Done") {
+                        amountIsFocused = false
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Rodrigo on 28-05-24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
